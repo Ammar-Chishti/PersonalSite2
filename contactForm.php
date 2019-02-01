701668fc3c64ff9f46f42dc66af18c65349a3e02
Ammar Chishti <ammarchishti80@gmail.com>
2019-02-01 03:52:20 -0500

    Create contact form (partially)

---
+++ contactForm.php
@@ -0,0 +1,15 @@
+<?php
+    $name = $_Post['name']
+    $visitor_email = $_POST['email']
+    $message = $_POST["message"]
+    $email_from = "ammarchishti80@gmail.com";
+    $email_subject = "New Form Submission";
+    $email_body = "User Name:  $name.\n".
+                        "User Email: $vistor_email.\n".
+                            "User Message: $message.\n".;
+    $to = "ammarchishti80@gmail.com";
+    $headers = "From: $email_from \r\n";
+    $headers .= "Reply-To: $visitor_email \r\n";
+    mail($to,$email_subject,$email_body,$headers);
+    header("Location: index.html");
+?>
